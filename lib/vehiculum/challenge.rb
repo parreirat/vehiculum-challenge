3debc1fcdf4c36fd3d6f0cca31f1f1b8406a5fea
Tiago Parreira <parreirat@gmail.com>
2018-06-24 12:23:58 +0100

    Initial bundle gem

---
+++ lib/vehiculum/challenge.rb
@@ -0,0 +1,7 @@
+require "vehiculum/challenge/version"
+
+module Vehiculum
+  module Challenge
+    # Your code goes here...
+  end
+end
---
+++ lib/vehiculum/challenge/version.rb
@@ -0,0 +1,5 @@
+module Vehiculum
+  module Challenge
+    VERSION = "0.1.0"
+  end
+end
